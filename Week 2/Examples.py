ce9b726644305d6bbcedb15f9e96f4bdedd21bdf
vladimirSirin <shadowsong_sirin@hotmail.com>
2018-05-28 22:35:16 +0800

    save the pdf files, practice and checking the codes in book

---
+++ Week 2/Examples.py
@@ -0,0 +1,40 @@
+"""Examples for the book"""
+
+
+def improve(update, close, guess=1):
+    while not close(guess):
+        guess = update(guess)
+    return guess
+
+
+def approx_eq(x, y, tolerance=1e-15):
+    return abs(x - y) < tolerance
+
+
+def newton_update(f, df):
+    def update(x):
+        return x - f(x) / df(x)
+    return update
+
+
+def find_zero(f, df):
+    def near_zero(x):
+        return approx_eq(f(x), 0)
+    return improve(newton_update(f, df), near_zero)
+
+
+"""We first implement square_root by defining f and its derivative df. 
+We use from calculus the fact that the derivative of f(x)=x^2 - a 
+is the linear function df(x) = 2x."""
+
+
+def square_root_newton(a):
+    def f(x):
+        return x * x - a
+
+    def df(x):
+        return x * 2
+    return find_zero(f, df)
+
+
+print(square_root_newton(64))
