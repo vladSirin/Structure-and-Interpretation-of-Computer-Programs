33a72b1bd85a4d4fc062fa13a223ad6bd815c3f2
vladimirSirin <shadowsong_sirin@hotmail.com>
2018-05-29 23:57:28 +0800

    Understanding how higher order functions actually mean and work, update the comments

--- Week 2/Examples.py
+++ Week 2/Examples.py
@@ -2,6 +2,10 @@
 
 
 def improve(update, close, guess=1):
+    """When a higher order function as this is called, it first creates the frame with the function and assign
+    the name of arguments with the actual functions are called.
+        Then in this case, resolve each of the assigned functions first before doing the computations inside
+    the higher order function."""
     while not close(guess):
         guess = update(guess)
     return guess
@@ -12,6 +16,8 @@ def approx_eq(x, y, tolerance=1e-15):
 
 
 def newton_update(f, df):
+    """When this function is used as an argument, it is executed so that
+    actually this is a function takes 1 argument as update(x) instead of 2 arguments as it was"""
     def update(x):
         return x - f(x) / df(x)
     return update
